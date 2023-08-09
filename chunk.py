7da3bd4d1fcb6489fee047231ae2a2fe2854f3fc
dash-uvic <53027798+dash-uvic@users.noreply.github.com>
2023-08-09 13:37:19 -0700

    Update chunk.py
    Added example of histogram and thresholding

--- chunk.py
+++ chunk.py
@@ -5,8 +5,17 @@ import numpy as np
 file = TODO #"drive/MyDrive/CSKS4_GEC_B_WR_01_VV_RA_FF_20191126085014_20191126085029.MBI.tif.zarr"
 data_array = xr.open_dataset(file, engine="zarr")
 
+threshold = 2000
+ds = data_array.where(data_array < threshold)
+
+bins = np.linspace(-1, threshold, 10)
+print(bins)
 def process_chunk(chunk):
-  pass
+  chunk = chunk.to_array()
+  #Generate histogram example for chunk
+  print(np.unique(chunk.to_numpy()))
+  result = chunk.groupby_bins(chunk, bins).apply(xr.DataArray.count).fillna(0)
+  return result
 
 def pyramid(data_array, levels=[500, 250, 100]):
   for chunksize in levels:
@@ -23,7 +32,10 @@ def pyramid(data_array, levels=[500, 250, 100]):
         #print(f"{idx[i]}, {idy[j]}")
         tgt_y = xr.DataArray(np.arange(idy[j], idy[j+1]), dims="y")
         chunk = data_array.isel(y=tgt_y, x=tgt_x, band=0)
-        do_something = process_chunk(chunk)
+        hist = process_chunk(chunk)
+        ####
+        # Do something with histogram
+        ####
         #print(da)
 
-pyramid(data_array)
+pyramid(ds)
